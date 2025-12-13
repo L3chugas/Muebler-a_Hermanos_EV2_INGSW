36be4a6d3e5dae8a3ed1c44219e1a6e29091e7d0
L3chugas <carlosbenjaminorellana@gmail.comm>
2025-12-12 21:23:33 -0300

    base de datos actualizada

--- base_de_datos_SQL/muebles_hermanos.sql
+++ base_de_datos_SQL/muebles_hermanos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-11-2025 a las 01:14:58
+-- Tiempo de generación: 13-12-2025 a las 01:22:55
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -39,8 +39,14 @@ CREATE TABLE `cotizacion` (
 --
 
 INSERT INTO `cotizacion` (`id_contizacion`, `fecha_cotizacion`, `calculo_total`, `estado_cotizacion`) VALUES
-(2, '2025-11-24 12:36:51', 0, 'PENDIENTE'),
-(10, '2025-11-24 13:37:27', 657000, 'PAGADA');
+(2, '2025-11-24 12:36:51', 0, 'PAGADA'),
+(10, '2025-11-24 13:37:27', 657000, 'PAGADA'),
+(15, '2025-11-24 23:13:35', 135000, 'PENDIENTE'),
+(17, '2025-12-12 20:24:51', 450000, 'PENDIENTE'),
+(18, '2025-12-12 20:27:25', 95000, 'PAGADA'),
+(19, '2025-12-12 20:37:53', 45000, 'PAGADA'),
+(20, '2025-12-12 20:41:41', 360000, 'PAGADA'),
+(21, '2025-12-12 21:13:05', 30000, 'PAGADA');
 
 -- --------------------------------------------------------
 
@@ -64,7 +70,15 @@ CREATE TABLE `detalle_cotizacion` (
 INSERT INTO `detalle_cotizacion` (`id_detalle_cotiz`, `cantidad`, `subtotal`, `cotizacion_id`, `mueble_id`, `variante_id`) VALUES
 (2, 2, 240000, 2, 1, 1),
 (11, 1, 395000, 10, 2, 5),
-(12, 1, 262000, 10, 2, 7);
+(12, 1, 262000, 10, 2, 7),
+(17, 2, 30000, 15, 1, 1),
+(18, 1, 105000, 15, 2, 3),
+(20, 2, 30000, 17, 1, NULL),
+(21, 28, 420000, 17, 1, NULL),
+(22, 1, 95000, 18, 2, 2),
+(23, 3, 45000, 19, 1, 1),
+(24, 5, 360000, 20, 3, 7),
+(25, 2, 30000, 21, 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -89,9 +103,13 @@ CREATE TABLE `mueble` (
 --
 
 INSERT INTO `mueble` (`id_mueble`, `nombre_mueble`, `tipo`, `precio_base`, `stock`, `estado_activo`, `tamano`, `material`, `tipo_mueble`) VALUES
-(1, 'Silla Clásica', 'Silla', 15000, 5, 1, 'MEDIANO', 'Madera', NULL),
-(2, 'Mesa Comedor', 'Mesa', 80000, 0, 1, 'GRANDE', 'Roble', NULL),
-(3, 'mesedora marca acme', NULL, 60000, 5, 1, 'GRANDE', 'Madera', 'Mesedora');
+(1, 'Silla Clásica', 'Silla', 15000, 3, NULL, NULL, 'Madera', ''),
+(2, 'Mesa Comedor', 'Mesa', 80000, 0, NULL, NULL, 'Roble', ''),
+(3, 'mesedora marca acme', NULL, 60000, 0, 1, 'GRANDE', 'Madera', 'Mesedora'),
+(8, 'Silla de Comedor Clásica', NULL, 45000, 15, 1, 'MEDIANO', 'Madera de Roble', 'Silla'),
+(10, 'silla de test', NULL, 155065, 50, NULL, NULL, 'Madera', 'silla'),
+(11, 'Gamer Escritorio', NULL, 99999, 1, NULL, NULL, 'pulento', 'Escritorio'),
+(12, 'silla de 4 patas', NULL, 5, 5, NULL, NULL, 'madera', 'silla');
 
 -- --------------------------------------------------------
 
@@ -116,7 +134,8 @@ INSERT INTO `variante` (`id_variante`, `nombre_variante`, `precio_adicional`) VA
 (4, 'Ruedas de Alto Tráfico', 5000),
 (5, 'Tapiz de Cuero', 45000),
 (6, 'Manillas Doradas', 3500),
-(7, 'Vidrio Templado', 12000);
+(7, 'Vidrio Templado', 12000),
+(8, 'Color Premium - Gris Oscuro', 15000);
 
 --
 -- Índices para tablas volcadas
@@ -157,25 +176,25 @@ ALTER TABLE `variante`
 -- AUTO_INCREMENT de la tabla `cotizacion`
 --
 ALTER TABLE `cotizacion`
-  MODIFY `id_contizacion` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_contizacion` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT de la tabla `detalle_cotizacion`
 --
 ALTER TABLE `detalle_cotizacion`
-  MODIFY `id_detalle_cotiz` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_detalle_cotiz` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de la tabla `mueble`
 --
 ALTER TABLE `mueble`
-  MODIFY `id_mueble` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_mueble` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `variante`
 --
 ALTER TABLE `variante`
-  MODIFY `id_variante` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_variante` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Restricciones para tablas volcadas
